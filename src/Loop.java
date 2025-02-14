dfb188aaddefb9f6bf629de92c0c8753b93271a3
charlesanderson25 <charles_spfc@hotmail.com>
2025-02-14 19:07:09 -0300

    Add Loop file

---
+++ src/Loop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        System.out.println("Digite sua avaliação para o filme: ");
+        double avaliacao = leitura.nextDouble();
+
+        for (int i = 0; i < 3; i++) {
+
+        }
+    }
+}
