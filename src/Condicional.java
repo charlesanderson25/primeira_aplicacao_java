e699eab888fb9d59e38389e4f8d1c74743c0d9bb
charlesanderson25 <charles_spfc@hotmail.com>
2025-02-14 18:32:38 -0300

    Update file

--- src/Condicional.java
+++ src/Condicional.java
@@ -3,5 +3,17 @@ public class Condicional {
         int anoLancamento = 2025;
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+        if (anoLancamento >= 2025){
+            System.out.println("O filme é lançamento");
+        } else {
+            System.out.println("O filme é antigo");
+        }
+        if (incluidoNoPlano == true && tipoPlano.equals("plus")){
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve pagar a locação!");
+        }
     }
 }
