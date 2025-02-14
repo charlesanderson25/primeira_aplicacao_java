915923cc00150b2f1f24106859ff48bf3bd62f36
charlesanderson25 <charles_spfc@hotmail.com>
2025-02-14 18:09:39 -0300

    Add Condicional file

---
+++ src/Condicional.java
@@ -0,0 +1,7 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoLancamento = 2025;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+    }
+}
