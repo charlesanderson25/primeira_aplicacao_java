8bf56f3ab164cfc3d7dcfd647405200968180b63
charlesanderson25 <charles_spfc@hotmail.com>
2025-02-15 19:22:58 -0300

    Update file

--- src/Projeto.java
+++ src/Projeto.java
@@ -10,6 +10,8 @@ public class Projeto {
         double consultarSaldos = 0;
         double deposito = 0;
         double transferencia = 0;
+        double saldoAtualizado = 0;
+
 
         //Scanner
         Scanner opcao = new Scanner(System.in);
@@ -31,18 +33,28 @@ public class Projeto {
         System.out.println("Digite a opção desejada: ");
         double opcaoEscolhida = opcao.nextDouble();
 
-        if (opcaoEscolhida == 1){
-            System.out.println("Saldo atualizado R$ " + saldoInicial);
-        } else if (opcaoEscolhida == 2) {
-            System.out.println("Informe o valor do depósito: ");
-            deposito = opcao.nextDouble();
-            double saldoAtualizado = deposito + Double.parseDouble(saldoInicial);
-            System.out.println("Saldo atualizado: R$ " + String.format("%.2f", saldoAtualizado));
-        } else if (opcaoEscolhida == 3) {
-            System.out.println("Informe o valor da transferência: R$ ");
-            transferencia = opcao.nextDouble();
-            double saldoAtualizado = transferencia - Double.parseDouble(saldoInicial);
-            System.out.println("Saldo atualizado: R$ " + String.format("%.2f", saldoAtualizado));
+        while (opcaoEscolhida != 4){
+
+            System.out.println("Operações: \n");
+            System.out.println("1- Consultar saldo");
+            System.out.println("2- Depósito");
+            System.out.println("3- Transferência");
+            System.out.println("4- Sair \n");
+
+            if (opcaoEscolhida == 1){
+                System.out.println("Saldo atualizado R$ " + saldoInicial);
+            } else if (opcaoEscolhida == 2) {
+                System.out.println("Informe o valor do depósito: ");
+                deposito = opcao.nextDouble();
+                saldoAtualizado = deposito + Double.parseDouble(saldoInicial);
+                System.out.println("Saldo atualizado: R$ " + String.format("%.2f", saldoAtualizado));
+            } else if (opcaoEscolhida == 3) {
+                System.out.println("Informe o valor da transferência: R$ ");
+                transferencia = opcao.nextDouble();
+                saldoAtualizado = transferencia - Double.parseDouble(saldoInicial);
+                System.out.println("Saldo atualizado: R$ " + String.format("%.2f", saldoAtualizado));
+            }
+            opcaoEscolhida++;
         }
 
     }
