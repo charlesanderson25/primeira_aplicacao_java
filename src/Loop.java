0d38810595ca4966518e836614cbd17b651fde4b
charlesanderson25 <charles_spfc@hotmail.com>
2025-02-15 12:14:50 -0300

    Update Loop.java file

--- src/Loop.java
+++ src/Loop.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class Loop {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
-        double mediaAvaliacao = 0;
+        double somaNotas = 0;
         double nota = 0;
+        double mediaAvaliacao = 0;
 
-        System.out.println("Digite sua avaliação para o filme: ");
-        double avaliacao = leitura.nextDouble();
+        //double avaliacao = leitura.nextDouble();
 
         for (int i = 0; i < 3; i++) {
+            System.out.println("Digite a nota do filme: ");
+            nota = leitura.nextDouble();
+            somaNotas = somaNotas + nota;
+            mediaAvaliacao = somaNotas / 3;
 
         }
+        System.out.println("A media das avaliacoes é de: " + mediaAvaliacao);
     }
 }
